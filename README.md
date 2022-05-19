910c86f6e76005d6873d042fb864805a8210d7ab
KunYuChang <mischiefsub@gmail.com>
2022-05-19 19:20:39 +0800

    v4 新增索引與資料庫連線

--- README.md
+++ README.md
@@ -37,7 +37,18 @@ VALUES ('Fist post','This is the first of many posts!', '2022-05-18 10:30:25'),
 VALUES ('Second post','Another interesting post!', '2022-05-18 10:35:25');
 ```
 
+使用索引:加快查詢速度
 
+✍️ 每當使用where的任何欄都需要加上索引
+```sql
+ALTER TABLE cms.article ADD INDEX (title);
+```
+
+## 資料庫連線
+
+[mysqli_connect](https://www.php.net/manual/en/function.mysqli-connect.php)
+[mysqli_extension](https://www.php.net/manual/en/book.mysqli.php)
+[mysqli_connect_error](https://www.php.net/manual/en/mysqli.connect-error.php)
 ## 專案來源
 
 此專案為 Dave Hollingworth 所開設的 Udemy 課程
--- index.php
+++ index.php
@@ -1,40 +1,15 @@
 <?php
 
-$articles = [
-    [
-        'title' => 'First post',
-        'content' => 'This is the first of many posts!'
-    ],
-    [
-        'title' => 'Another',
-        'content' => 'Yet another fascinating post...'
-    ],
-    [
-        'title' => 'Read this!',
-        'content' => 'You must read this now, it\' essential reading!'
-    ]
-];
-?>
+$db_host = 'localhost';
+$db_name = 'cms';
+$db_user = 'cms_www';
+$db_pass = 'cms_pass';
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-</head>
-<body>
-    <main>
-        <ul>
-            <?php foreach ($articles as $article): ?>
-                <li>
-                    <h2><?= $article['title']?></h2>
-                    <p><?= $article['content']?></p>
-                </li>
-            <?php endforeach;?>
-        </ul>
-    </main>
-</body>
-</html>
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
+
+echo "Connected successfully";
