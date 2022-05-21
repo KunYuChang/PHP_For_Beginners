e8f426c54d5ee16748941b3b77a6dcf5aabf45d5
KunYuChang <mischiefsub@gmail.com>
2022-05-21 14:18:36 +0800

    v25 後端驗證輸入值不能空白並顯示

--- new-article.php
+++ new-article.php
@@ -3,28 +3,40 @@
 // 資料庫連線
 require 'includes/database.php';
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $conn = getDB();
+    if ($_POST['title'] == '') {
+        $errors[] = 'Title is required';
+    }
 
-    $sql = "INSERT INTO article (title, content, published_at)
+    if ($_POST['content'] == '') {
+        $errors[] = 'Content is required';
+    }
+
+    if (empty($errors)) {
+        $conn = getDB();
+
+        $sql = "INSERT INTO article (title, content, published_at)
             VALUES (?,?,?)";
 
-    /** @var mysqli $conn */
-    $stmt = mysqli_prepare($conn, $sql);
+        /** @var mysqli $conn */
+        $stmt = mysqli_prepare($conn, $sql);
 
-    //判斷查詢結果是否成功
-    if ($stmt === false) {
-        echo mysqli_error($conn);
-    } else {
-
-        mysqli_stmt_bind_param($stmt, "sss", $_POST['title'], $_POST['content'], $_POST['published_at']);
-
-        if (mysqli_stmt_execute($stmt)) {
-            $id = mysqli_insert_id($conn);
-            echo "Inserted record with ID: $id";
+        //判斷查詢結果是否成功
+        if ($stmt === false) {
+            echo mysqli_error($conn);
         } else {
-            echo mysqli_st_error($stmt);
+
+            mysqli_stmt_bind_param($stmt, "sss", $_POST['title'], $_POST['content'], $_POST['published_at']);
+
+            if (mysqli_stmt_execute($stmt)) {
+                $id = mysqli_insert_id($conn);
+                echo "Inserted record with ID: $id";
+            } else {
+                echo mysqli_st_error($stmt);
+            }
         }
     }
 }
@@ -33,6 +45,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <?php require 'includes/header.php'; ?>
 
 <h2>New article</h2>
+
+<?php if (!empty($errors)) : ?>
+    <ul>
+        <?php foreach ($errors as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
 <form action="" method="post">
     <div>
