e01418bb79277b6e3a66de98546e317bb98f993e
KunYuChang <mischiefsub@gmail.com>
2022-05-22 14:45:06 +0800

    v36 清除Session, Cookie

--- logout.php
+++ logout.php
@@ -1,5 +1,19 @@
 <?php
 
-session_status();
+require 'includes/url.php';
 
-$_SESSION['is_logged_in'] = false;
+session_start();
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+session_destroy();
+
+redirect('/');
